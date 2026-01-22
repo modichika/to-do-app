596252f8c2157876ee4dc7cbf4bc6e48b00870a5
Shristi modi <shristimodi95@gmail.com>
2026-01-22 12:07:17 +0530

    added specific columns

--- lib/data.ts
+++ lib/data.ts
@@ -5,7 +5,7 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: require});
 
 export async function fetchTask(){
     try{
-    const data = await sql<Task[]>`SELECT * FROM tasks`;
+    const data = await sql<Task[]>`SELECT id, name, description FROM tasks`;
     return data;
     } catch(error){
         console.error('Database Error:', error);
