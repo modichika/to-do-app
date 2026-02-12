8fd87567bcc0b61ebdb39c882654ef35eb97dac0
Shristi modi <shristimodi95@gmail.com>
2026-02-12 22:57:16 +0530

    corrected yml  version

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export',      // Required: Tells Next.js to create the 'out' folder
+  images: {
+    unoptimized: true,  // Required: GitHub Pages doesn't support the Image Optimization API
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
